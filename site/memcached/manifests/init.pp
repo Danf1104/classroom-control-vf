81ad2c052d9ea2e07b7b678be7fc69096f2ee583
Danf1104 <dan.fleming.cdl9@statefarm.com>
2016-05-24 14:19:18 -0500

    Lab 11.1.0
    added memcached class

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,23 @@
+class memcached {
+
+  package { 'memcached':
+    ensure => present,
+  }
+
+  file { 'memcached':
+    ensure  => file,
+    path    => '/etc/sysconfig/memcached',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'],
+  }
+  
+  service { 'memcached':
+    ensure  => running,
+    enable  => true,
+    subscribe => File['memcached'],
+  }
+
+}
