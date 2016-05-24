547993c4823130533dd80c0c0b2be7edb745cd55
Danf1104 <dan.fleming.cdl9@statefarm.com>
2016-05-24 13:17:35 -0500

    Lab 9.3
    updated skeleton class

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,11 +1,16 @@
 class skeleton {
 
   file { '/etc/skel':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
+  }
+  file { '/etc/skel/.bashrc':
     ensure => file,
     owner => 'root',
     group => 'root',
-    mode => '0440',
-    source => 'puppet:///modules/sudo/sudoers',
+    mode => '0640',
+    source => 'puppet:///modules/skeleton/bashrc',
   }
-
 }
