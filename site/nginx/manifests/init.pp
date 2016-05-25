a24130aeb7fb47884b939544ff01cae391d1d9d5
Danf1104 <dan.fleming.cdl9@statefarm.com>
2016-05-25 08:54:42 -0500

    Lab 11.2
    Updated nginx class with additional files and service

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -11,4 +11,49 @@ class nginx {
     provider => rpm,
     require  => File['nginx rpm'],
   }
+    file { '/var/www':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { '/var/www/index.html':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  file { '/etc/nginx/conf.d':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['nginx'],
+  }
 }
